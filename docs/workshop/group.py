c7839f648e966fa8a2c56d135bf51b1c37646b7b
Silvio Peroni <essepuntato@gmail.com>
2023-01-05 12:49:00 +0100

    added license

--- docs/workshop/group.py
+++ docs/workshop/group.py
@@ -1,3 +1,18 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2019, Silvio Peroni <essepuntato@gmail.com>
+#
+# Permission to use, copy, modify, and/or distribute this software for any purpose
+# with or without fee is hereby granted, provided that the above copyright notice
+# and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+# REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND
+# FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT,
+# OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE,
+# DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS
+# ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS
+# SOFTWARE.
+
 # This is a fake (i.e. it fails) implementation of the 'do_move' 
 # function, that does always select an invalid couple of coordinates
 # as next move to run. Change the body of the function to provide 
